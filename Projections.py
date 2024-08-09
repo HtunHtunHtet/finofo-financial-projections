c1432e159902737e7825b9771d99116abadc0d3e
Htun Htun Htet <ryanhhh91@gmail.com>
2024-08-08 21:06:12 -0600

    Introduce projections class

---
+++ Projections.py
@@ -0,0 +1,50 @@
+import File
+import pandas as pd
+from typing import List, Dict
+from datetime import  timedelta
+
+
+class Projections:
+    def __init__(self, file: File) -> None:
+        self._file: File = file
+
+    def generate_sales_projections(self, initial_sales: float, growth_rate: float) -> List[Dict[str, str]]:
+        historical_date = self._file.historical_date()
+        start_date = pd.to_datetime(historical_date)
+
+        projections = []
+        current_sales = initial_sales
+
+        for i in range(1, 13):
+            date = start_date + timedelta(days=30 * i)
+            current_sales *= (1 + growth_rate)
+            projections.append({
+                'date': date.strftime('%Y-%m-%d %H:%M:%S'),
+                'sales': f'{current_sales:.2f}'
+            })
+
+        return projections
+
+    def generate_product_sales_projections(self) -> List[Dict[str, str]]:
+        initial_product_sales = self._file.get_product_sales_value()
+        return self.generate_sales_projections(initial_product_sales, 0.04)
+
+    def generate_service_sales_projections(self) -> List[Dict[str, str]]:
+        initial_service_sales = self._file.get_service_sales_value()
+        return self.generate_sales_projections(initial_service_sales, 0.05)
+
+    def generate_combined_projections(self) -> List[Dict[str, str]]:
+        product_sales_projections = self.generate_product_sales_projections()
+        service_sales_projections = self.generate_service_sales_projections()
+
+        combined_projections = []
+        for product, service in zip(product_sales_projections, service_sales_projections):
+            total_sales = float(product['sales']) + float(service['sales'])
+            combined_projections.append({
+                'date': product['date'],
+                'product_sales': product['sales'],
+                'service_sales': service['sales'],
+                'total_sales': f'{total_sales:.2f}'
+            })
+
+        return combined_projections
--- read_file.py
+++ read_file.py
@@ -1,4 +1,12 @@
 from File import File
+from Projections import Projections
 
 file = File('financial_projections.xlsx', 'Sheet1')
 file.read_file()
+
+projections = Projections(file)
+
+combined_projections = projections.generate_combined_projections()
+
+for projection in combined_projections:
+    print(projection)
